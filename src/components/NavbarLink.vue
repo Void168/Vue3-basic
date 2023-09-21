80562e0b1c966d9ccd59dba16f6ea3927b61a629
LPHung <phuhung16820@gmail.com>
2023-09-21 22:45:35 +0700

    first commit

---
+++ src/components/NavbarLink.vue
@@ -0,0 +1,30 @@
+<template>
+  <a
+    class="nav-link"
+    :class="activeClasses"
+    aria-current="page"
+    :title="`This link goes to the ${page.link.text} page`"
+    :href="page.link.url"
+    >{{ page.link.text }}</a
+  >
+</template>
+
+<script>
+export default {
+  props: ["page", "isActive"],
+  computed: {
+    activeClasses() {
+      return {
+        active: this.isActive,
+        emphasize: this.isActive,
+      };
+    },
+  },
+};
+</script>
+
+<style scoped>
+.emphasize {
+  text-decoration: underline !important;
+}
+</style>
